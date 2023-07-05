5e7c402375259ef580f53c687c6ce3b08553a0ef
ConnerKT <connerthompson121@gmail.com>
2023-07-05 14:50:35 -0500

    finished challenge

--- c-sharp/CodeChallenge1/ArrayReverse.cs
+++ c-sharp/CodeChallenge1/ArrayReverse.cs
@@ -5,8 +5,14 @@ namespace CodeChallenges
     {
         public static int[] ReverseArray(int[] array)
         {
-
-            return array;
+      int[] newArr = new int[array.Length];
+      int taLength = newArr.Length;
+      
+        for (int x = array.Length; x > 0; x--) {
+          newArr[taLength - x] = array[x - 1];
+              }
+      
+            return newArr;
         }
     }
 }
