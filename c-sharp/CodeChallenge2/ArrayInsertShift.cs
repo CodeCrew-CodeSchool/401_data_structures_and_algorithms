c3a94641896ed81448b30f1395cb5599cac3dbfa
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-06 21:08:15 -0500

    Challenge 2

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -4,8 +4,28 @@ namespace CodeChallenges
     {
         public static int[] InsertShiftArray(int[] array, int value)
         {
-            //insert the value into the middle of the array
-            return array;
+      //insert the value into the middle of the array
+      int middleIndex = arr.Length / 2;
+      int[] newArray = new int[arr.Length + 1];
+
+      for (int i = 0; i < newArray.Length; i++)
+      {
+        if (i < middleIndex)
+        {
+          newArray[i] = arr[i];
+        }
+        else if (i == middleIndex)
+        {
+          newArray[i] = value;
+        }
+        else
+        {
+          newArray[i] = arr[i - 1];
+        }
+      }
+
+      return newArray;
+
         }
     }
 }
