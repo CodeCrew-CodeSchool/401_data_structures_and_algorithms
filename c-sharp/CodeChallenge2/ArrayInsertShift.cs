a452c13f9f2f2180a0db6101dce423071c971084
ConnerKT <connerthompson121@gmail.com>
2023-07-08 19:56:55 -0500

    process of completing challenge
    pushing to meet ACP requirements

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -1,11 +1,39 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge2
+  public class CodeChallenge2
+  {
+    public static int[] InsertShiftArray(int[] array, int value)
     {
-        public static int[] InsertShiftArray(int[] array, int value)
+      int halfPoint = 0;
+
+      if (array.Length % 2 == 0)
+      {
+        halfPoint = array.Length / 2;
+      }
+      else
+      {
+        halfPoint = array.Length / 2 + 1;
+      }
+
+      int[] newArr = new int[array.Length + 1];
+
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (i < halfPoint)
         {
-            //insert the value into the middle of the array
-            return array;
+          newArr[i] = array[i];
         }
+        else if (i == halfPoint)
+        {
+          newArr[i] = value;
+        }
+        else
+        {
+          newArr[i + 1] = array[i];
+        }
+      }
+
+      return newArr;
     }
+  }
 }
