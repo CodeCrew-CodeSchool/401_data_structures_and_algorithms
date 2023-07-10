6a043db0bf1e221afb95080738116f2b142fa96a
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-07-10 17:41:21 -0500

    submit code challenge 2

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -7,16 +7,16 @@ namespace CodeChallenges
     {
       int[] resultArray = new int[array.Length + 1];
 
-      int shiftIndex = array.Length / 2;
+      int shiftIndex = (resultArray.Length - 1) / 2;
 
       for (int i = 0; i < resultArray.Length; i++)
       {
-        if(i < shiftIndex)
+        if (i < shiftIndex)
         {
           resultArray[i] = array[i];
 
         }
-        else if(i == shiftIndex)
+        else if (i == shiftIndex)
         {
           resultArray[shiftIndex] = value;
 
@@ -31,13 +31,5 @@ namespace CodeChallenges
       return resultArray;
     }
 
-    //static void Main()
-    //{
-    //  int[] testArr = { 5, 1, 4, 7 };
-    //  int[] arr = InsertShiftArray(testArr, 3);
-    //  Console.WriteLine($"{arr[0]},{arr[1]},{arr[2]},{arr[4]}");
-    //  Console.ReadLine();
-    //}
   }
 }
-
