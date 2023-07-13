1178ada596d94a8d8e25f659b9fdb7e1b252eecf
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-12 19:46:38 -0500

    Update ArrayBinarySearch.cs

--- c-sharp/CodeChallenge3/ArrayBinarySearch.cs
+++ c-sharp/CodeChallenge3/ArrayBinarySearch.cs
@@ -1,11 +1,35 @@
-namespace CodeChallenges 
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeChallenges
 {
-    public class CodeChallenge3
+  public class CodeChallenge3
+  {
+    public static int BinarySearch(int[] sortedArray, int searchKey, int min, int max)
     {
-        public static int BinarySearch(int[] array, int key)
-        {
-            int foundElementIndex = -1;
-            return foundElementIndex;
-        }
+
+      // int low = 0;
+      // int mid = sortedArray.Length/2;
+      // int high = sortedArray.Length -1;
+
+      int mid = (min + max) / 2;
+
+      if (searchKey == sortedArray[mid])
+      {
+        return mid;
+      }
+      else if (searchKey < sortedArray[mid])
+      {
+         max = mid - 1;
+        mid = (min + max) / 2;
+      }
+      else if (searchKey > sortedArray[mid])
+      {
+        min = mid + 1;
+        mid = (min + max) / 2;
+      }
+    int  foundElementIndex = BinarySearch(sortedArray, searchKey, min, max);
+      
+      return foundElementIndex;
     }
+  }
 }
