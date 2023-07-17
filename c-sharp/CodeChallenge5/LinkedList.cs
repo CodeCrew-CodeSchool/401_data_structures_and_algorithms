34b7dee14c10d25e7cf26dd7ff229cc8ebcef093
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-17 16:09:48 -0500

    code challenge 5

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,13 +1,33 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class LinkedList
+  public class LinkedList
+  {
+    public Node Head { get; set; }
+
+    public void Insert(int value)
     {
-        // public Node Head {get; set;}
+      Node newNode = new Node (value);
 
+      if (Head == null)
+      {
+        Head = newNode;
+      }else
+      {
+        newNode.Next = Head;
+        Head = newNode;
+      }
     }
+  }
 
-    public class Node
+  public class Node
+  {
+    public int Value { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int value)
     {
-
+      Value = value;
+      Next = null;
     }
+  }
 }
