874f3c684944c997eb4d74fecbd01ed6bbb40931
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-07-17 16:41:22 -0500

    trying to fix git stuff
    added linked list code and tried to fix stuff

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -1,1 +1,43 @@
 
+namespace CodeChallenges
+{
+  public class CodeChallenge4
+  {
+    public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
+    {
+      if (nthNumberInSequence == 0)
+      {
+        return 0;
+      }
+
+      int prevNumber = 0, prevPrevNumber = 1, currentNumber = 1;
+
+      for (int i = 1; i < nthNumberInSequence; i++)
+      {
+        prevPrevNumber = prevNumber;
+        prevNumber = currentNumber;
+        currentNumber = prevNumber + prevPrevNumber;
+      }
+
+      return currentNumber;
+    }
+
+    public static int[] RowSums(int[][] matrix)
+    {
+      int[] rowSums = new int[matrix.Length];
+
+      for (int i = 0; i < matrix.Length; i++)
+      {
+        int sum = 0;
+        for (int j = 0; j < matrix[0].Length; j++)
+        {
+          sum += matrix[i][j];
+        }
+        rowSums[i] = sum;
+      }
+
+      return rowSums;
+    }
+
+  }
+}
--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,13 +1,62 @@
 namespace CodeChallenges 
 {
-    public class LinkedList
-    {
-        // public Node Head {get; set;}
+  public class LinkedList
+  {
+    public Node Head;
 
+    public void AddNewNode(Node node)
+    {
+      if (Head != null)
+      {
+        node.Next = Head;
+        Head = node;
+
+      }
+      else
+      {
+        Head = node;
+      }
     }
 
-    public class Node
+    public bool Includes(int value)
     {
+      Node current = Head;
+      while (current != null)
+      {
+        if (current.Value == value)
+        {
+          return true;
+        }
 
+        current = current.Next;
+      }
+
+      return false;
     }
+
+    public override string ToString()
+    {
+      Node current = Head;
+      string outputStr = "";
+      while (current != null)
+      {
+        outputStr += $"{current.Value} -> ";
+        current = current.Next;
+      }
+
+      outputStr += "NULL";
+      return outputStr;
+    }
+
+  }
+
+  public class Node
+  {
+    public Node Next;
+    public int Value;
+    public Node(int value)
+    {
+      Value = value;
+    }
+  }
 }
