6d71ebb6316e80b93ac657493b854ce53af1a746
ConnerKT <connerthompson121@gmail.com>
2023-07-17 20:32:06 -0500

    update

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -8,6 +8,13 @@ namespace CodeChallenges
 
     public class Node
     {
+        public int Data;
+        public Node Next;
 
+    public Node(int data)
+    {
+        Data = data;
+        Next = null;
+    }
     }
 }
