efea1a6dbdee561726db382f2b369f4f7fbfb4c0
ConnerKT <connerthompson121@gmail.com>
2023-07-17 21:01:45 -0500

    added methods

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,20 +1,34 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class LinkedList
+  public class LinkedList
+  {
+    public Node Head { get; set; }
+
+    public void insert(int data)
     {
-        // public Node Head {get; set;}
+      //Adds a new node with that value to the head of the
+      Node Node = new Node(data);
+    }
+    public bool includes(int data)
+    {
+      //returns true if that value exists as a node's value somewhere within the list
+      //We have to check every element in the list
+    }
+    public string toString()
+    {
 
     }
+  }
 
-    public class Node
-    {
-        public int Data;
-        public Node Next;
+  public class Node
+  {
+    public int Data;
+    public Node Next;
 
     public Node(int data)
     {
-        Data = data;
-        Next = null;
+      Data = data;
+      Next = null;
     }
-    }
+  }
 }
