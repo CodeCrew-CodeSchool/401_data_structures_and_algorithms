0470a762b25853376b5ca026f167930af01851ca
ConnerKT <connerthompson121@gmail.com>
2023-07-18 18:48:20 -0500

    ch5 finished

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -8,15 +8,48 @@ namespace CodeChallenges
     {
       //Adds a new node with that value to the head of the
       Node Node = new Node(data);
+
+      if(Head == null)
+      {
+        Head = Node;
+      }else
+      {
+        Node.Next = Head;
+        Head = Node;
+      }
     }
     public bool includes(int data)
     {
       //returns true if that value exists as a node's value somewhere within the list
       //We have to check every element in the list
-    }
-    public string toString()
-    {
+      bool result = false;
 
+      Node currentNode = Head;
+
+      while(currentNode != null)
+      {
+        if (currentNode.Data == data)
+        {
+          result = true;
+          break;
+        }
+        currentNode = currentNode.Next;
+      }
+
+      return result;
+    }
+    public string ToString()
+    {
+      string liststring = "";
+      Node currentNode = Head;
+
+      while (currentNode != null)
+      {
+        liststring += String.Format("{0} -> ", currentNode.Data);
+        currentNode = currentNode.Next;
+      }
+
+      return liststring + "NULL";
     }
   }
 
