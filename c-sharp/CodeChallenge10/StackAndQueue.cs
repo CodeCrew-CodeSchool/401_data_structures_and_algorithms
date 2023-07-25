4f77615b4906dde12b8167acb97a6e5801dd1dcf
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-07-25 18:38:49 -0500

    submit challenge 9

--- c-sharp/CodeChallenge10/StackAndQueue.cs
+++ c-sharp/CodeChallenge10/StackAndQueue.cs
@@ -1,16 +1,66 @@
 namespace CodeChallenges
 {
-    public class Node
-    {
+  public class Node
+  {
+    public Node Next { get; set; }
+    public int Value { get; set; }
 
+    public Node(int val)
+    {
+      Value = val;
+    }
+  }
+
+  public class Stack
+  {
+    public Node? Top { get; set; } = null;
+
+    public void Push(int val)
+    {
+      Node newNode = new Node(val);
+      
+      if(Top != null)
+      {
+        Top.Next = newNode;
+        Top = newNode;
+      }
+      Top = newNode;
     }
 
-    public class Stack
+    public int Pop()
     {
+      if (Top == null)
+      {
+        throw new Exception("Stack is empty");
+      }
 
+      int result = Top.Value;
+      Top = Top.Next;
+      return result;
     }
 
-    public class Queue
+    public int Peek()
     {
+      if(Top == null)
+      {
+        throw new Exception("Stack is empty");
+      }
+      return Top.Value;
     }
+
+    public bool IsEmpty()
+    {
+      if (Top == null)
+      {
+        return true;
+      }
+      return false;
+    }
+  }
+
+
+  public class Queue
+  {
+
+  }
 }
