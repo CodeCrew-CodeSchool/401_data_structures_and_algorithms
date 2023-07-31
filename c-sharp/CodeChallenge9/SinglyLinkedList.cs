7cd1f8cf913a6ca73497f637ea5ac0ba75065640
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-31 10:12:20 -0500

    IsPalindrome

--- c-sharp/CodeChallenge9/SinglyLinkedList.cs
+++ c-sharp/CodeChallenge9/SinglyLinkedList.cs
@@ -2,15 +2,49 @@ namespace CodeChallenges
 {
   public class CodeChallenge9
   {
-    public static LinkedListKth ReverseLinkedList(LinkedListKth list)
+    public static void ReverseLinkedList(LinkedListKth list)
     {
-      return list;
+      Node previous = null;
+      Node current = list.Head;
+      Node temp = null;
+
+      while (current != null)
+      {
+        temp = current.Next;
+        current.Next = previous;
+        previous = current;
+        current = temp;
+      }
+
+      list.Head = previous;
     }
 
     public static bool IsPalindrome(LinkedListKth list)
     {
-      bool isPalindrome = false;
-      return isPalindrome;
+      LinkedListKth copyList = new LinkedListKth();
+      Node node = list.Head;
+      while (node != null)
+      {
+        Node newNode = new Node(node.Value);
+        newNode.Next = copyList.Head;
+        copyList.Head = newNode;
+        node = node.Next;
+      }
+
+      Node originalCurrent = list.Head;
+      Node copyCurrent = copyList.Head;
+
+      while (originalCurrent != null && copyCurrent != null)
+      {
+        if (originalCurrent.Value != copyCurrent.Value)
+        {
+          return false;
+        }
+        originalCurrent = originalCurrent.Next;
+        copyCurrent = copyCurrent.Next;
+      }
+
+      return originalCurrent == null && copyCurrent == null;
     }
   }
 }
