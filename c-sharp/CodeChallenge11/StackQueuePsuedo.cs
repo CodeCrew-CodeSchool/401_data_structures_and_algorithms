61078d971584c536228714275c22077f0aee201b
Chyniece Matthews <cfmatthews2@gmail.com>
2023-08-02 12:10:17 -0500

    lab 11

--- c-sharp/CodeChallenge11/StackQueuePsuedo.cs
+++ c-sharp/CodeChallenge11/StackQueuePsuedo.cs
@@ -1,42 +1,39 @@
 using Xunit;
 namespace CodeChallenges
 {
-  public class StackQueuePsuedo
+  public class PseudoQueue
   {
-   public class PseudoQueue
+    private Stack stack1;
+    private Stack stack2;
+
+    public PseudoQueue()
     {
+      stack1 = new Stack();
+      stack2 = new Stack();
+    }
 
-      private Stack stack1;
-      private Stack stack2;
-
-      public PseudoQueue()
+    public void Enqueue(int value)
+    {
+      while (!stack1.IsEmpty())
       {
-        stack1 = new Stack();
-        stack2 = new Stack();
+        stack2.Push(stack1.Pop());
       }
 
-      public void Enqueue(int value)
+      stack1.Push(value);
+
+      while (!stack2.IsEmpty())
       {
-        while (stack2.Count >0)
-        {
-          stack2.Push(stack1.Pop());
-        }
-
-        stack1.Push(value);
-
-        while(stack2.Count > 0)
-        {
-          stack1.Push(stack2.Pop());
-        }
+        stack1.Push(stack2.Pop());
       }
+    }
 
-      public int Dequeue()
+    public int Dequeue()
+    {
+      if (stack1.IsEmpty())
       {
-        if (stack1.Count == 0)
-        {
-          throw new InvalidOperationException("The Pseudo is empty");
-        }
-        return (int)stack1.Pop();
+        throw new InvalidOperationException("The PseudoQueue is empty");
       }
+      return stack1.Pop();
+    }
   }
 }
