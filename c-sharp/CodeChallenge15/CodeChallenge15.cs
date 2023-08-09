136fab78bafb137654cf30669df08677ebcb89c7
rgreen32 <ray.greenm@gmail.com>
2023-08-09 14:39:51 -0700

    cc15

---
+++ c-sharp/CodeChallenge15/CodeChallenge15.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+namespace CodeChallenges
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public int[] PreOrderSearch()
+        {
+            // LinkList<int> values = new L
+
+
+        }
+    }
+}
---
+++ c-sharp/CodeChallenge15/CodeChallenge15Test.cs
@@ -0,0 +1,14 @@
+using CodeChallenges;
+using Xunit;
+
+namespace CodeChallenge15Tests
+{
+  public class CodeChallenge15Test
+  {
+    [Fact]
+    public void PreOrderSearch_Test()
+    {
+      Assert.Equal(true, true);
+    }
+  }
+}
