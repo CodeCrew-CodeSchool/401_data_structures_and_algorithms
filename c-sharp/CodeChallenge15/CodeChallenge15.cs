eed6fd8c94c29cfe80500b30607b4eabccd37c49
rgreen32 <ray.greenm@gmail.com>
2023-08-14 16:38:34 -0700

    cc15

--- c-sharp/CodeChallenge15/CodeChallenge15.cs
+++ c-sharp/CodeChallenge15/CodeChallenge15.cs
@@ -13,89 +13,9 @@ namespace CodeChallenges
     }
     public class BinaryTree
     {
-        public Node Root { get; set; }
         public LinkedList<int> PreOrderSearch()
         {
-            LinkedList<int> values = new LinkedList<int>();
-            Stack<Node> traversalStack = new Stack<Node>();
-
-            if (Root != null)
-            {
-                traversalStack.Push(Root);
-
-                while (traversalStack.Count != 0)
-                {
-                    Node currentNode = traversalStack.Pop();
-                    values.AddLast(currentNode.Value);
-
-                    // Note the order of pushing to the stack is different.
-                    // First, push the right child, then the left child.
-                    if (currentNode.Right != null)
-                    {
-                        traversalStack.Push(currentNode.Right);
-                    }
-                    if (currentNode.Left != null)
-                    {
-                        traversalStack.Push(currentNode.Left);
-                    }
-                }
-            }
-            return values;
-        }
-    public override string ToString()
-    {
-        string treeString = "";
-
-        Dictionary<int, LinkedList<Node?>> levels = new Dictionary<int, LinkedList<Node?>>();
-        levels.Add(0, new LinkedList<Node?>());
-        levels[0].AddLast(Root);
-        int level = 0;
-
-        while(true)
-        {
-            int nextLevel = level + 1;
-            LinkedList<Node?> currentLevel = levels[level];
-            levels.Add(nextLevel, new LinkedList<Node?>());
-            foreach(Node? node in currentLevel)
-            {
-                levels[nextLevel].AddLast(node?.Left);
-                levels[nextLevel].AddLast(node?.Right);
-            }
-            
-            bool isNotLastLevel = (from node in levels[nextLevel]
-                                where node != null
-                                select node).Any();
-            if(isNotLastLevel == false) { break; }
-            else { level += 1; }
-        }
-        int numberOfSpaces = 0;
-        for(int i = levels.Count-1; i > 0; i--)
-        {
-            string branchString = "";
-            string line = "";
-            for(int x = 0; x < numberOfSpaces; x++){ line += " "; }
-            foreach(Node node in levels[i])
-            {
-                line += (node?.Value != null) ? node.Value : "n";
-                line += "   ";
-            }
-            Console.WriteLine(line);
-        }
-        return "";
-    }
-    public void PrintInOrder(Node node)
-        {
-            if (node == null)
-                return;
-
-            // Traverse the left subtree
-            PrintInOrder(node.Left);
-
-            // Print the current node's value
-            Console.Write(node.Value + " ");
-
-            // Traverse the right subtree
-            PrintInOrder(node.Right);
+            return new LinkedList<int>();
         }
     }
 }
--- c-sharp/CodeChallenge15/CodeChallenge15Test.cs
+++ c-sharp/CodeChallenge15/CodeChallenge15Test.cs
@@ -1,49 +1,90 @@
 using CodeChallenges;
 using Xunit;
-
+using System.Reflection;
 namespace CodeChallengeTests
 {
   public class CodeChallenge15Test
   {
+    public PropertyInfo LeftNodeProperty;
+    public PropertyInfo RightNodeProperty;
+    public PropertyInfo ValueNodeProperty;
+    public PropertyInfo BinaryTreeRootProperty;
+    public ConstructorInfo NodeConstructorMethod;
+    public ConstructorInfo BinaryTreeConstructorMethod;
 
+    public CodeChallenge15Test()
+    {
+      var nodeType = typeof(Node);
+      var binaryTree = typeof(BinaryTree);
+      NodeConstructorMethod = nodeType.GetConstructor(new Type[] { typeof(int) });
+      BinaryTreeConstructorMethod = binaryTree.GetConstructor(new Type[] { });
+      BinaryTreeRootProperty = binaryTree.GetProperty("Root");
+      LeftNodeProperty = nodeType.GetProperty("Left");
+      RightNodeProperty = nodeType.GetProperty("Right");
+      ValueNodeProperty = nodeType.GetProperty("Value");
+    }
+
+    [Fact]
+    public void Node_Class_Has_Left_Property()
+    {
+      Assert.NotNull(LeftNodeProperty);
+    }
+
+    [Fact]
+    public void Node_Class_Has_Right_Property()
+    {
+      Assert.NotNull(RightNodeProperty);
+    }
+    [Fact]
+    public void Node_Class_Has_Value_Property()
+    {
+      Assert.NotNull(ValueNodeProperty);
+    }
+    [Fact]
+    public void Node_Class_Has_Constructor_Method()
+    {
+      Assert.NotNull(NodeConstructorMethod);
+    }
     [Fact]
     public void PreOrderSearch_Test()
     {
-      BinaryTree tree = new BinaryTree();
-      tree.Root = new Node(1);
-      tree.Root.Left = new Node(2);
-      tree.Root.Right = new Node(3);
-      tree.Root.Left.Left = new Node(5);
-      tree.Root.Left.Right = new Node(8);
+      BinaryTree tree = (BinaryTree)BinaryTreeConstructorMethod.Invoke(null);
+      Node Root = (Node)NodeConstructorMethod.Invoke(new object[] { 1 });
+      BinaryTreeRootProperty.SetValue(tree, Root);
+
+      LeftNodeProperty.SetValue(BinaryTreeRootProperty.GetValue(tree), (Node)NodeConstructorMethod.Invoke(new object[] { 2 }));
+      RightNodeProperty.SetValue(BinaryTreeRootProperty.GetValue(tree), (Node)NodeConstructorMethod.Invoke(new object[] { 3 }));
+      LeftNodeProperty.SetValue((Node)LeftNodeProperty.GetValue(BinaryTreeRootProperty.GetValue(tree)), (Node)NodeConstructorMethod.Invoke(new object[] { 5 }));
+
+      RightNodeProperty.SetValue((Node)LeftNodeProperty.GetValue(BinaryTreeRootProperty.GetValue(tree)), (Node)NodeConstructorMethod.Invoke(new object[] { 8 }));
 
       LinkedList<int> result = tree.PreOrderSearch();
 
       int[] expectedValues = { 1, 2, 5, 8, 3 };
-      int i = 0;
-      foreach (int value in result)
-      {
-        Assert.Equal(expectedValues[i], value);
-        i++;
-      }
+      Assert.Equal(expectedValues, result);
     }
 
     [Fact]
     public void PreOrderSearch_EmptyTree_Test()
     {
-      BinaryTree tree = new BinaryTree();
+      BinaryTree tree = (BinaryTree)BinaryTreeConstructorMethod.Invoke(null);
 
-      LinkedList<int> result = tree.PreOrderSearch();
+      LinkedList<int> result = (LinkedList<int>)typeof(BinaryTree)
+          .GetMethod("PreOrderSearch")
+          .Invoke(tree, null);
 
       Assert.Empty(result);
     }
-
     [Fact]
     public void PreOrderSearch_SingleNodeTree_Test()
     {
-      BinaryTree tree = new BinaryTree();
-      tree.Root = new Node(42);
+      BinaryTree tree = (BinaryTree)BinaryTreeConstructorMethod.Invoke(null);
+      Node rootNode = (Node)NodeConstructorMethod.Invoke(new object[] { 42 });
+      BinaryTreeRootProperty.SetValue(tree, rootNode);
 
-      LinkedList<int> result = tree.PreOrderSearch();
+      LinkedList<int> result = (LinkedList<int>)typeof(BinaryTree)
+          .GetMethod("PreOrderSearch")
+          .Invoke(tree, null);
 
       Assert.Single(result);
       Assert.Equal(42, result.First.Value);
@@ -53,19 +94,25 @@ namespace CodeChallengeTests
     public void PreOrderSearch_LargeTree_Test()
     {
       // Create a large binary tree for testing
-      BinaryTree tree = new BinaryTree();
-      tree.Root = new Node(1);
-      Node currentNode = tree.Root;
-      for (int i = 2; i <= 6; i+=2)
+      BinaryTree tree = (BinaryTree)BinaryTreeConstructorMethod.Invoke(null);
+      Node rootNode = (Node)NodeConstructorMethod.Invoke(new object[] { 1 });
+      BinaryTreeRootProperty.SetValue(tree, rootNode);
+
+      Node currentNode = rootNode;
+      for (int i = 2; i <= 6; i += 2)
       {
-        currentNode.Left = new Node(i);
-        currentNode.Right = new Node(i + 1);
-        currentNode = currentNode.Left;
+        LeftNodeProperty.SetValue(currentNode, (Node)NodeConstructorMethod.Invoke(new object[] { i }));
+        RightNodeProperty.SetValue(currentNode, (Node)NodeConstructorMethod.Invoke(new object[] { i + 1 }));
+        currentNode = (Node)LeftNodeProperty.GetValue(currentNode);
       }
-      LinkedList<int> result = tree.PreOrderSearch();
+
+      LinkedList<int> result = (LinkedList<int>)typeof(BinaryTree)
+          .GetMethod("PreOrderSearch")
+          .Invoke(tree, null);
+
       LinkedList<int> expectedResult = new LinkedList<int>(new[] { 1, 2, 4, 6, 7, 5, 3 });
-      // Verify that the first few values match the expected sequence
       Assert.Equal(expectedResult, result);
     }
+
   }
 }
