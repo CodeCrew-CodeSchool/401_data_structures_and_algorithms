b31e082701f11374e5cde9465f0b7510ed88ac2f
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-08-13 20:48:13 -0500

    submit challenge 13

--- c-sharp/CodeChallenge13/StackQueueBrackets.cs
+++ c-sharp/CodeChallenge13/StackQueueBrackets.cs
@@ -1,10 +1,69 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge13
+  public class CodeChallenge13
+  {
+    public static bool ValidateBrackets(string syntax)
     {
-        public static bool ValidateBrackets(string syntax)
+      Stack<char> validator = new();
+
+      foreach (char c in syntax)
+      {
+        if (c == '(' || c == '{' || c == '[')
         {
-            return false;
+          validator.Push(c);
         }
+
+        if (c == ')')
+        {
+          if (validator.Count == 0) return false;
+          if (validator.Peek() == '(')
+          {
+            validator.Pop();
+          }
+          else
+          {
+            return false;
+          }
+        }
+
+        if (c == '}')
+        {
+          if (validator.Count == 0) return false;
+          if (validator.Peek() == '{')
+          {
+            validator.Pop();
+          }
+
+          else
+          {
+            return false;
+          }
+        }
+
+        if (c == ']')
+        {
+          if (validator.Count == 0) return false;
+          if (validator.Peek() == '[')
+          {
+            validator.Pop();
+          }
+
+          else
+          {
+            return false;
+          }
+        }
+
+      }
+
+      if(validator.Count == 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
+  }
 }
