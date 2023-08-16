740240e788a5d2af3e7b83078a1a077ff90b8641
rgreen32 <ray.greenm@gmail.com>
2023-08-16 07:48:52 -0700

    cc15

---
+++ c-sharp/CodeChallenge16/CodeChallenge16.cs
@@ -0,0 +1,10 @@
+namespace CodeChallenges
+{
+    public class CodeChallenge16
+    {
+        public static void BreadthFirst(BinaryTree tes)
+        {
+            
+        }
+    }
+}
---
+++ c-sharp/CodeChallenge16/CodeChallenge16Test.cs
@@ -0,0 +1,14 @@
+using CodeChallenges;
+using Xunit;
+
+namespace CodeChallengeTests
+{
+  public class CodeChallenge16Tests
+  {
+    [Fact]
+    public void BreadthFirst_Test()
+    {
+      Assert.Equal(true, true);
+    }
+  }
+}
